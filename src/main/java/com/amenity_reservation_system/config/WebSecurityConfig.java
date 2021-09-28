c20ff93d9a9f8f990c3e1ae83cc4f7b0387a3742
DaLei Sun <sundalei1988@gmail.com>
2021-09-28 14:25:36 +0800

    add security feature

--- src/main/java/com/amenity_reservation_system/config/WebSecurityConfig.java
+++ src/main/java/com/amenity_reservation_system/config/WebSecurityConfig.java
@@ -1,14 +1,26 @@
 package com.amenity_reservation_system.config;
 
+import com.amenity_reservation_system.service.UserDetailsServiceImpl;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
