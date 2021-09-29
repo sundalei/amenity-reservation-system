2ef381d29b5e1f703752e3e23d6fd11667796ff6
DaLei Sun <dalei.sun@infor.com>
2021-09-29 13:56:23 +0800

    add capacity checking feature

---
+++ src/main/java/com/amenity_reservation_system/model/Capacity.java
@@ -0,0 +1,35 @@
+package com.amenity_reservation_system.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Capacity {
+    @Id
+    @Column(nullable = false, updatable = false)
+    @SequenceGenerator(
+        name = "primary_sequence",
+        sequenceName = "primary_sequence",
+        allocationSize = 1,
+        initialValue = 10000
+    )
+
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "primary_sequence"
+    )
+    private Long id;
+}
